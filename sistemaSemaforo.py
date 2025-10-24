367c415c477d78c60f7a172a9c65ad892426c69e
Enzo Wacker Guinossi <enzowackerguinossi@gmail.com>
2025-10-23 21:44:17 -0300

    AAA

--- sistemaSemaforo.py
+++ sistemaSemaforo.py
@@ -9,6 +9,35 @@ ruas = [
 ]
 
 def limpar_tela():
-    os.system 
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-def main():
+def representar_ruas():
+    for rua in ruas:
+        if(rua["aberta"]):
+            simbolo = '|   |'
+        else: simbolo = '| X |'
+    print(simbolo, end="  ")
+
+def representar_carros():
+    for rua in ruas:
+        print(f"Rua {rua['nome']}: {rua['qtd_carros']} carros", end="  ")
+        print("\n")
+
+def simular():
+    indice_aberto = 0
+    while True:
+        limpar_tela()
+        representar_ruas()
+        representar_carros()
+
+        aberta = ruas[indice_aberto]
+        aberta["qtd_carros"] = max(0, aberta["qtd_carros"] - random.randint(1, 3))
+
+        ruas[indice_aberto]["aberta"] = False
+        indice_aberto = (indice_aberto + 1) % len(ruas)
+        ruas[indice_aberto]["aberta"] = True
+
+        time.sleep(1)
+
+if __name__ == "__main__":    
+    simular()
